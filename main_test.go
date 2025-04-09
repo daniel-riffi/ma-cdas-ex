f754bacc42c37ab3c6715aa1477381500a7c42cd
Daniel Riffert <riffert.daniel@gmail.com>
2025-04-09 19:01:47 +0200

    updated table creation query in test

--- main_test.go
+++ main_test.go
@@ -44,6 +44,8 @@ const tableCreationQuery = `CREATE TABLE IF NOT EXISTS products
     id SERIAL,
     name TEXT NOT NULL,
     price NUMERIC(10,2) NOT NULL DEFAULT 0.00,
+    created_at TIMESTAMP DEFAULT NOW(),
+    stock INT DEFAULT 10,
     CONSTRAINT products_pkey PRIMARY KEY (id)
 )`
 
